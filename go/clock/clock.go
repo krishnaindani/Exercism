bee4c2e694f7fbc00ed4900bbfc6be07dd9ceb83
Krishna Indani <indanikrishna@gmail.com>
2020-05-12 07:31:56 -0700

    basic setup

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,4 +1,27 @@
 package clock
 
-type Clock int 
+//Clock type
+type Clock int
 
+//New created a new clock
+func New(hout, minute int) Clock {
+	var c Clock
+	return c
+}
+
+//String return representation of the clock
+func (c *Clock) String() string {
+	return ""
+}
+
+//Add adds minutes to the clock
+func (c *Clock) Add(minutes int) Clock {
+	var cl Clock
+	return cl
+}
+
+//Subtract subtracts the minutes from clock
+func (c *Clock) Subtract(minutes int) Clock {
+	var cl Clock
+	return cl
+}
