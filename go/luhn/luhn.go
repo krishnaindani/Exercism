b328ce6e238b549550fd9a4f9390cc01fc216b7c
Krishna Indani <indanikrishna@gmail.com>
2020-05-22 08:34:44 -0700

    removing repetation

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,7 +1,6 @@
 package luhn
 
 import (
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -9,33 +8,28 @@ import (
 //Valid checks if the credit card number is valid or not
 func Valid(number string) bool {
 	//strip the spaces
-	number = strings.Replace(number, " ", "", -1)
+	number = strings.ReplaceAll(number, " ", "")
 	l := len(number)
-
-	//checks if number container characters apart from numbers
-	r := regexp.MustCompile("^[0-9]*$")
-	validNumber := r.Match([]byte(number))
-	if !validNumber || l < 2 {
+	if l < 2 {
 		return false
 	}
 
 	var toDouble bool
 	var total int
 	for i := l - 1; i >= 0; i-- {
-		n, _ := strconv.Atoi(string(number[i]))
-		if toDouble {
-			double := n * 2
-			if double > 9 {
-				double = double - 9
-			}
-			total += double
-			toDouble = false
-		} else {
-			total += n
-			toDouble = true
+		n, err := strconv.Atoi(string(number[i]))
+		if err != nil {
+			return false
 		}
+		if toDouble {
+			n *= 2
+			if n > 9 {
+				n -= 9
+			}
+		}
+		total += n
+		toDouble = !toDouble
 	}
-
 	return total%10 == 0
 
 }
