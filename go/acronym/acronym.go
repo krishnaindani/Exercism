010b41eded0bbfcad9f8360a7398e28d74dd452e
Krishna Indani <indanikrishna@gmail.com>
2020-11-09 18:53:17 -0800

    acronym solution

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,7 +1,16 @@
 package acronym
 
+import (
+	"strings"
+)
+
 // Abbreviate should have a comment documenting it.
 func Abbreviate(s string) string {
 	var short string
+	for _, v := range strings.FieldsFunc(s, func(c rune) bool {
+		return c == '_' || c == ' ' || c == '-'
+	}) {
+		short += strings.ToUpper(string(v[0]))
+	}
 	return short
 }
