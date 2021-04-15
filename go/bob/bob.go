6745656f46fbd0ed6279dda2cc99047ca6bdec00
Krishna Indani <indanikrishna@gmail.com>
2021-04-14 22:00:25 -0700

    bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,34 @@
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"strings"
+	"unicode"
+)
+
+// Hey returns the response for the conversation
 func Hey(remark string) string {
-	var output string
-	switch remark {
-	case remark.IsUpper() == true:
-		output = "Sure"
-	case "WATCH OUT!":
-		output = "Whoa, chill out!"
+
+	remark = strings.TrimSpace(remark)
+
+	switch {
+	case len(remark) == 0:
+		return "Fine. Be that way!"
+	case strings.ToUpper(remark) == remark && remark[len(remark)-1] == '?' && containsCharacter(remark):
+		return "Calm down, I know what I'm doing!"
+	case strings.ToUpper(remark) == remark && containsCharacter(remark):
+		return "Whoa, chill out!"
+	case remark[len(remark)-1] == '?':
+		return "Sure."
 	default:
-		output = "Whatever."
+		return "Whatever."
 	}
-	return output
+}
+
+func containsCharacter(s string) bool {
+	for _, c := range s {
+		if unicode.IsLetter(c) {
+			return true
+		}
+	}
+	return false
 }
