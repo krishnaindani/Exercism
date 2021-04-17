baa144018589c4e20ce7446a02b7f69875cb5bb3
Krishna Indani <indanikrishna@gmail.com>
2021-04-16 20:27:15 -0700

    add question

---
+++ go/list-ops/list_ops.go
@@ -0,0 +1,15 @@
+package listops
+
+type IntList []int
+
+func (i IntList) Append(list []int) IntList {
+	return nil
+}
+
+func (i IntList) Reverse() IntList {
+	return nil
+}
+
+func Filter() {
+
+}
